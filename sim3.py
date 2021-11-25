c947ec3e41f53ed7d3a16a927f472c6d4fcfb2c7
Nordin Zakaria <nordinzakaria@gmail.com>
2021-11-25 14:09:54 +0800

    updated something

--- sim3.py
+++ sim3.py
@@ -1,8 +1,11 @@
 import threading
 import time
 import random
+import numpy as np
 from queue import Queue
 
+
+cusList = []
 
 simTime = 0
 simEnd = 20
@@ -10,24 +13,41 @@ cusIndex = 0
 
 class IceCreamCustomer (threading.Thread):  # User is a Thread
 
-    def __init__(self, name, icecreamBrand, reputation):
+    def __init__(self, name, icecreamBrand, reputation, location):
         threading.Thread.__init__(self)
-        self.name = username
+        self.name = name
         self.icecreamBrand = icecreamBrand
+        self.reputation = reputation
+
+        self.location = location
         print('Customer ', self.name, ' created')
         
     def run(self):
         global simTime, simEnd
         
         while simTime < simEnd:
-              time.sleep(1)
+            for customer in cusList:
+
+
+
+                if self.reputation > customer.reputation:
+                    continue
+                else:
+                    print('Customer ', self.name, ' changing preference to ', self.icecreamBrand)
+                    self.icecreamBrand = customer.icecreamBrand
+
+            time.sleep(1)
         
  
 numCustomers = 5
 for c in range(numCustomers):
+
+    loc = 
+
     cusObj = IceCreamCustomer("AJ"+str(cusIndex), random.randint(1, 5), random.randint(1,10))
     cusIndex = cusIndex + 1
 
+    cusList.append(cusObj)
     cusObj.start()
 
 
